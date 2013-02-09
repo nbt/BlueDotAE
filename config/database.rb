be35e8b8af54a68ba994cab74d94420d9a748f0c
Robert Poor <rdpoor@gmail.com>
2013-02-08 21:52:16 -0800

    correcting postgresql access, adding admin

--- config/database.rb
+++ config/database.rb
@@ -8,12 +8,14 @@
 # # A Sqlite3 connection
 # DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "development.db"))
 #
-
 DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 
+user = ENV['POSTGRESQL_USERNAME'] || 'root'
+pass = ENV['POSTGRESQL_PASSWORD'] || ''
+
 case Padrino.env
-  when :development then DataMapper.setup(:default, "postgres://root@localhost/blue_dot_ae_development")
-  when :production  then DataMapper.setup(:default, "postgres://root@localhost/blue_dot_ae_production")
-  when :test        then DataMapper.setup(:default, "postgres://root@localhost/blue_dot_ae_test")
+when :development then DataMapper.setup(:default, "postgres://#{user}:#{pass}@localhost/blue_dot_ae_development")
+when :production  then DataMapper.setup(:default, "postgres://#{user}:#{pass}@localhost/blue_dot_ae_production")
+when :test        then DataMapper.setup(:default, "postgres://#{user}:#{pass}@localhost/blue_dot_ae_test")
 end
