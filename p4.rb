29c3e1c60841576920c35e0e1478f5f6c69e96b5
Robert Poor <rdpoor@gmail.com>
2013-03-14 00:31:59 -0700

    deleted a couple of stragglers.

--- p4.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'config/boot.rb'
-trap("SIGINT") { debugger }
-# Get daily climate interquartile mean for stations near to Premise.first
-Climate.daily_climate(Premises.first)
-true
-
